afd328b1963db1345982bb96b21e908258feefcf
Gang <vison@collectorx.io>
2024-09-30 20:13:36 +0700

    Update api

--- application/config/constants.php
+++ application/config/constants.php
@@ -85,8 +85,9 @@ defined('EXIT__AUTO_MIN')      OR define('EXIT__AUTO_MIN', 9); // lowest automat
 defined('EXIT__AUTO_MAX')      OR define('EXIT__AUTO_MAX', 125); // highest automatically-assigned error code
 
 // APIs config
-define('API_URL', 'land-bn/api/');
-// define('API_URL', 'http://bn.watbundanjai.com/api/');
+// define('API_URL', 'land-bn/api/');
+define('API_URL', 'https://bn.landdefender.org/api/');
+
 define('API_TOKEN', '1234');
 const KNOWLEDGE_CATE = [
   '1'=>'การออกแบบวัด',
